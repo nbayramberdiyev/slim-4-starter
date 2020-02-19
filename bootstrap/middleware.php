bd4b49184c84e61e6be0d5fa92a469df1257f8f0
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2020-02-20 00:13:57 +0300

    Replace Slim's default error handler with Whoops

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -3,17 +3,12 @@
 declare(strict_types=1);
 
 use Slim\Views\TwigMiddleware;
-use Slim\Exception\HttpNotFoundException;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Psr7\Response;
 use App\Middleware\Session;
+use Zeuxisoo\Whoops\Slim\WhoopsMiddleware;
 
 $app->addRoutingMiddleware();
 
-$app->addErrorMiddleware(true, true, true)->setErrorHandler(HttpNotFoundException::class, function(Request $request, $exception) use ($container) {
-    $response = new Response;
-    return $container->get('view')->render($response->withStatus(404), 'errors/404.twig');
-});
+$app->add(new WhoopsMiddleware);
 
 $app->add(TwigMiddleware::createFromContainer($app));
 
