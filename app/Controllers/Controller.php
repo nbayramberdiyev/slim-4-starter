707347a53cd7ab728a985f032c46c88f83f6c1ed
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2020-03-19 01:03:38 +0300

    Add docblock to base controller class.

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -6,15 +6,32 @@ namespace App\Controllers;
 
 use Psr\Container\ContainerInterface;
 
+/**
+ * Base Controller class.
+ *
+ * @property \Slim\Views\Twig $view
+ */
 abstract class Controller
 {
+    /**
+     * @var ContainerInterface $container
+     */
     protected $container;
 
+    /**
+     * Base Controller constructor.
+     *
+     * @param ContainerInterface $container
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
+    /**
+     * @param  mixed $property
+     * @return mixed|null
+     */
     public function __get($property)
     {
         if ($this->container->has($property)) {
