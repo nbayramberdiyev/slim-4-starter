2767dab0740fa57ffed6e474dba331c83cac10e2
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-01-11 11:59:29 +0300

    Update route callable definitions

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Controllers\ContactController;
 
 return function (Slim\App $app): void
 {
-    $app->get('/', HomeController::class . ':index')->setName('home.index');
-    $app->get('/about', AboutController::class . ':index')->setName('about.index');
-    $app->get('/contact', ContactController::class . ':index')->setName('contact.index');
+    $app->get('/', [HomeController::class, 'index'])->setName('home.index');
+    $app->get('/about', [AboutController::class, 'index'])->setName('about.index');
+    $app->get('/contact', [ContactController::class, 'index'])->setName('contact.index');
 };
