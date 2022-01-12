fea1d6db593aaa4395f813212426ea51c18deb35
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-01-12 19:22:29 +0300

    Add testing (w/ PHPUnit)

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
