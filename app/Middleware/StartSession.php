0c1236b2612817caed76808459f242899a2606a4
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-01-20 08:51:16 +0300

    Implement middleware interface

--- app/Middleware/StartSession.php
+++ app/Middleware/StartSession.php
@@ -6,11 +6,12 @@ namespace App\Middleware;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface as Middleware;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
-class StartSession
+class StartSession implements Middleware
 {
-    public function __invoke(Request $request, RequestHandler $handler): Response
+    public function process(Request $request, RequestHandler $handler): Response
     {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
