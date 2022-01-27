ceb21d9330b547bb54208d1dc22fa01de8c8a56b
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-01-27 16:47:25 +0300

    Add helper methods

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -35,4 +35,35 @@ abstract class Controller
 
         return $this->twig->render($response, $template, $data);
     }
+
+    /**
+     * Creates a JSON response.
+     *
+     * @param Response $response
+     * @param mixed    $data
+     * @param int      $status
+     * @param int      $flags
+     *
+     * @return Response
+     */
+    protected function json(Response $response, mixed $data, int $status = 200, int $flags = 0): Response
+    {
+        $response->getBody()->write((string) json_encode($data, $flags));
+
+        return $response->withStatus($status)->withHeader('Content-Type', 'application/json');
+    }
+
+    /**
+     * Creates a redirect response.
+     *
+     * @param Response $response
+     * @param string   $url
+     * @param int      $status
+     *
+     * @return Response
+     */
+    protected function redirect(Response $response, string $url, int $status = 302): Response
+    {
+        return $response->withStatus($status)->withHeader('Location', $url);
+    }
 }
