58c47a30e32b30aba2fee546e64275616a879f6e
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-02-04 12:04:49 +0300

    Remove whoops error handler

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use App\Middleware\StartSession;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
-use Zeuxisoo\Whoops\Slim\WhoopsMiddleware;
 
 return function (Slim\App $app): void {
     $app->addBodyParsingMiddleware();
@@ -15,6 +14,4 @@ return function (Slim\App $app): void {
     $app->add(TwigMiddleware::createFromContainer($app, Twig::class));
 
     $app->add(new StartSession());
-
-    $app->add(new WhoopsMiddleware());
 };
