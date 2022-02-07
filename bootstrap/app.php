40a68f21db0f6efa51d454ec633dc167619d89c8
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-02-07 09:09:50 +0300

    Move dotenv to index

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 use DI\Container;
 use Slim\Factory\AppFactory;
-use Dotenv\Dotenv;
 
 $container = new Container();
 
 AppFactory::setContainer($container);
-
-Dotenv::createImmutable(dirname(__DIR__))->load();
 
 $app = AppFactory::create();
 
--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 Spatie\Ignition\Ignition::make()->register();
 
+Dotenv\Dotenv::createImmutable(dirname(__DIR__))->load();
+
 $app = require __DIR__ . '/../bootstrap/app.php';
 
 (require __DIR__ . '/../bootstrap/middleware.php')($app);
