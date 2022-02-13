606c547185f7f32e125d5a77d292c8f7a943e2e9
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-02-13 13:19:31 +0300

    Add helper functions

---
+++ app/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Gets the value of an environment variable.
+ *
+ * @param  string $key
+ * @param  mixed  $default
+ *
+ * @return mixed
+ */
+function env(string $key, mixed $default = null): mixed
+{
+    if (isset($_ENV[$key])) {
+        return match (strtolower($_ENV[$key])) {
+            'true'  => true,
+            'false' => false,
+            'null'  => null,
+            default => $_ENV[$key],
+        };
+    }
+
+    return $default;
+}
