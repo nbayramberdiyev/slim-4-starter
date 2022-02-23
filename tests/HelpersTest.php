bdd6956a329431af68ccc1774f9489613d39360c
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-02-23 22:41:10 +0300

    Add tests for helpers

---
+++ tests/HelpersTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function App\env;
+
+final class HelpersTest extends TestCase
+{
+    public function testEnv(): void
+    {
+        $_SERVER['foo'] = 'bar';
+        $this->assertSame('bar', env('foo'));
+
+        $_SERVER['foo'] = 'true';
+        $this->assertTrue(env('foo'));
+
+        $_SERVER['foo'] = 'false';
+        $this->assertFalse(env('foo'));
+
+        $_SERVER['foo'] = 'null';
+        $this->assertNull(env('foo'));
+
+        $_SERVER['foo'] = '';
+        $this->assertEmpty(env('foo'));
+    }
+
+    public function testEnvWithDefault()
+    {
+        $_SERVER['foo'] = 'bar';
+        $this->assertSame('bar', env('foo', 'default'));
+
+        $_SERVER['foo'] = '';
+        $this->assertSame('', env('foo', 'default'));
+
+        $_SERVER['foo'] = null;
+        $this->assertSame('default', env('foo', 'default'));
+
+        unset($_SERVER['foo']);
+        $this->assertSame('default', env('foo', 'default'));
+    }
+}
