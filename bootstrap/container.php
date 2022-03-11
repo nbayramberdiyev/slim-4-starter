eb308550c198cd1c8195227e4d2da8bf4d4b95be
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-03-12 00:52:47 +0300

    Add translations support

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -5,10 +5,25 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 use Psr\Container\ContainerInterface;
 use Slim\Views\Twig;
+use Symfony\Bridge\Twig\Extension\TranslationExtension;
+use Symfony\Component\Translation\Loader\JsonFileLoader;
+use Symfony\Component\Translation\Translator;
 
 $definitions = [
     'settings' => function () {
         return require 'settings.php';
+    },
+
+    Translator::class => function (ContainerInterface $container) {
+        $locale = $_SESSION['locale'] ?? $container->get('settings')['app']['locale'];
+        $translator = new Translator($locale);
+        $translator->addLoader('json', new JsonFileLoader());
+
+        if (file_exists($file = __DIR__ . '/../lang/' . $locale . '.json')) {
+            $translator->addResource('json', $file, $locale);
+        }
+
+        return $translator;
     },
 
     Twig::class => function (ContainerInterface $container) {
@@ -18,7 +33,13 @@ $definitions = [
             'cache' => $settings['view']['cache'],
         ];
 
-        return Twig::create($settings['view']['path'], $options);
+        $twig = Twig::create($settings['view']['path'], $options);
+        $translator = $container->get(Translator::class);
+
+        $twig->getEnvironment()->addGlobal('locale', $translator->getLocale());
+        $twig->addExtension(new TranslationExtension($translator));
+
+        return $twig;
     },
 ];
 
